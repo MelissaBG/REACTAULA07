5d29eea8c5b6822c1c0602af163eeecd8f3aef72
Melissa Bernardes Goulart <91767583+MelissaBG@users.noreply.github.com>
2022-06-24 21:51:37 -0300

    Atualizado.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,48 @@
 //Redfield por padrão mas que seja possível parametrizar o nome
 //da família.
 
+import { useState } from "react";
 import "./styles.css";
-const personagens = [
-  "Jill Valentine",
-  "Claire Redfield",
-  "Ada Wong",
-  "Chris Redfield",
-  "Leon S. Kennedy",
-  "Albert Wesker"
-];
+export default function App() {
+  const [pesquisa, setPesquisa] = useState("");
+  const personagens = [
+    "Jill Valentine",
+    "Claire Redfield",
+    "Ada Wong",
+    "Chris Redfield",
+    "Leon S. Kennedy",
+    "Albert Wesker"
+  ];
+  const filteredPersonagem = (personagem) => {
+    const familiaUpper = pesquisa.toUpperCase();
+    return personagem.toUpperCase().includes(familiaUpper);
+  };
+  const mapearPersonagem = (personagem) => <li>{personagem}</li>;
 
-export default function App(props) {
-  const personagem = props.personagem;
-  const nomeFamilia = personagem.familia;
-
+  const onClickHandler = (Event) => {
+    setPesquisa(event.target.value);
+  };
   return (
     <div>
       <h3>Personagens de Resident Evil</h3>
-      <ul>
-        {personagens
-          .filter((personagem) => personagem.includes("Redfield"))
-          .map((filteredPersonagem) => (
-            <li>{filteredPersonagem}</li>
-          ))}
-      </ul>
+      <input onChange={onClickHandler} />
+      <ul> {personagens.filter(filteredPersonagem).map(mapearPersonagem)} </ul>
     </div>
   );
 }
+const App2 = () => {
+  const [numero, setNumero] = useState();
+  const incrementoCallback = () => {
+    setNumero(numero + 1);
+    localStorage.setItem("numero", numero);
+  };
+  console.log("App2 executado");
+  return (
+    <div>
+      <p>Numero de vezes clicado: {numero}</p>
+      <button onClick={incrementoCallback}>Incremento</button>
+    </div>
+  );
+};
+
+export default App();
